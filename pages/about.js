556025fadd78c9fed65385458e79ba7d1bb1bcd9
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2022-05-09 08:50:25 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,10 +4,10 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ReactMarkdown from "react-markdown";
 import styles from '../styles/About.module.css'
-
+{/*that markdown library needs to imported*/}
 const URL =process.env.STRAPIBASEURL;
 
-
+{/*the query is slightly different than the post query*/}
 export async function getStaticProps() {
 
     const fetchParams ={
@@ -28,14 +28,14 @@ export async function getStaticProps() {
 
     const res = await fetch(`${URL}/graphql`, fetchParams);
     const {data} = await res.json();
-
-
+    {/*about contents is the items within the about section of the blog*/}
+    {/*revalidate requests new information from the server, in this case, strapi, every 10 secs*/}
     return {
         props: data.aboutcontents[0],
         revalidate: 10
     };
 }
-
+{/*similar composition as of the structure of slugjs, here you are also destructuring the body props*/}
 function About({body}) {
     return (
         <ContentWrapper>
