23c3cb0543d4de5178cb919c2bfc9e8bf684abcf
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2022-05-08 23:00:23 -0400

    Update ContentWrapper.js

--- components/ContentWrapper.js
+++ components/ContentWrapper.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Styles from '../styles/ContentWrapper.module.css'
-
+/*this is taking in children as a prop, you will then use div that renders that prop */
+/*compoments are made using this established method. They are thus easy to maintain and reuse*/
 function ContentWrapper({children}) {
     return (
         <div className={Styles.container}>
