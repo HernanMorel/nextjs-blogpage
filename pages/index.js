e3b442dfe9e16ba4c6dba7476fdc283310d50c0d
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2022-05-08 22:55:18 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,12 @@ import ContentWrapper from '../components/ContentWrapper'
 import Header from '../components/Header'
 import Blog from '../components/Blog'
 import Footer from '../components/Footer'
-//breakout as many components as possible, make it easier to find errors and debug if necessary 
+//the url const contains the base url from strapi, this making it accesible here
 const URL = process.env.STRAPIBASEURL
+//breakout as many components as possible, make it easier to find errors and debug if necessary 
 
 //getStaticProps generates HTML and JSON files, since the data is coming from strapi, it needs to be fed to the front-end through this method.
+//this in itself is an asynchronous function that makes it easier to get what you need from data sources
 export async function getStaticProps(context) {
  //the method is post since it is transpilling the data from the cms
   const fetchParams = {
@@ -35,6 +37,7 @@ export async function getStaticProps(context) {
 //graphql to view and parse the date from strapi,
 //the fetch() makes it possible to access resources asynchronously across the network.
   const res = await fetch(`${URL}/graphql`, fetchParams);
+ //fetchparams brings back JSON that needs to be unpacked
   const { data } = await res.json();
 
   return {
@@ -44,6 +47,7 @@ export async function getStaticProps(context) {
   };
 }
 //keep the vercel assests as they are, the cms implementation is what will be the most challenging part of this.
+// the blogposts are being fetched from the api through destructuring
 export default function Home({ blogposts }) {
   return (
     <ContentWrapper>
@@ -66,6 +70,7 @@ export default function Home({ blogposts }) {
       must edit.*/}
         <div className={styles.grid}>
          {/*This will map through the title, description, and slug data from strapi*/}
+         {/*Through a successful breakdown of the api data, it is now being displayed on the homepage.*/}
           {blogposts.map((blog, i) => {
             const { title, description, slug } = blog;
             //through the destructuring of variables, the fron end receives the data as it is filled in strapi.
