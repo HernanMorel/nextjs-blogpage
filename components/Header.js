904f8990537f49c5ae315a189b09d47dedd4cc9b
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2022-05-08 23:23:13 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link';
 import Styles from '../styles/Header.module.css'
+//using the shortcut rfc will return a template for a react functional component
+//link is next js substitute for react router and client-side route transitions
 
 function Header() {
     return (
@@ -10,11 +12,13 @@ function Header() {
                 <Link href='/'>
                     <a className={Styles.lnk}>
                         <button className={Styles.btn}>
+                            {/*this takes the name of the button just like regular html*/}
                             H.M. Blog
                         </button>
                     </a>
                 </Link>
             </div>
+{/*in Next, the a tag gets the style and the link link tag get the route href*/}
             <Link href='/'>
                 <a className={Styles.lnk}>
                     Home
